8f2ebe550e1b654af2b04f31b472a6c62c364a97
CJ <chirag7jain@gmail.com>
2013-01-16 14:50:37 +0530

    Improved crawler. Bug Fixes. Added support for iframe & frame tags

--- Crawler.py
+++ Crawler.py
@@ -5,6 +5,7 @@ import urlparse
 import signal
 from optparse import OptionParser
 
+#Getting Requested Page
 def GetPage(url):
 	try:
 		REQ = requests.get(url, timeout = 2)
@@ -13,22 +14,30 @@ def GetPage(url):
 			if contentType.startswith('text/html'):
 				return REQ.text
 			else:
-				return -1
-		else:
-			return 0
-	except:
-		print url + ' Please Check Url'
-		return 0
+				return -5
+	
+	except requests.ConnectionError:
+		return -1
+	except requests.Timeout:
+		return -2
+	except requests.HTTPError:
+		return -3
+	except requests.RequestException:
+		return -4
+		
+	return 0
 
-def GetAnchorTags(html):
+def GetTag(html,tag='a'):
+	#Getting Content Residing Under Given Tags
 	parsedHtml = BeautifulSoup.BeautifulSoup(html)
-	ATAGS = parsedHtml.findAll('a')
-	return ATAGS
+	VALUES = parsedHtml.findAll(tag)
+	return VALUES
 
-def GetProperLinks(aTags,currentUrl):
+def GetProperLinks(tag,currentUrl,option='href'):
+	#Extracting Links From Tags
 	LINKS = []
-	for link in aTags:
-		href = link.get('href')
+	for link in tag:
+		href = link.get(option)
 		if href !=None:		
 			if not href.startswith('http'):
 				if not IsAbsUrl(href):
@@ -39,7 +48,20 @@ def GetProperLinks(aTags,currentUrl):
 			LINKS.append(href)
 	return LINKS
 
+def GetAllLinks(html,url):
+	#Merging Links List Fetched From Anchor,IFrame,Frame Tags
+	LinkTags = GetTag(html,'a')
+	hrefs = GetProperLinks(LinkTags,url,'href')
+	LinkTags = GetTag(html,'iframe')
+	isrcs = GetProperLinks(LinkTags,url,'src')
+	LinkTags = GetTag(html,'frame')
+	srcs = GetProperLinks(LinkTags,url,'src')
+	LINKS = hrefs + isrcs + srcs
+	return LINKS
+
 def LinkCall(internalUrl,hrefs):
+	#Removing External Links
+	#By Base Path - Input URL
 	LINKS = []
 	for internalLinks in hrefs:
 		if internalLinks.startswith(internalUrl):
@@ -49,27 +71,52 @@ def LinkCall(internalUrl,hrefs):
 def IsAbsUrl(url):
 	return bool(urlparse.urlparse(url).scheme)
 
+def RequestIssue(issue):
+	if issue == -1:
+		return 'Network Issue'
+	elif issue == -2:
+		return 'TimeOut'
+	elif issue == -3:
+		return 'Bad/Invalid HTTP Response'
+	elif issue == -4:
+		return 'Something Weird'
+	elif issue == -5:
+		return 'Content Type'
+	return 'Fetched'
+
 class Crawle:
 	def __init__(self,values):
+		#Setting Options Values
 		self.url = values.url
 		self.count = values.count
 		self.ext = values.ext
 		self.repo = []
 		
 	def CheckList(self,item):
-		if item in self.repo:
-			return 0
+		#Checking Current Crawled URL's Against Repo To Avoid Duplicates
+		for value in self.repo:
+			if value[0]==item:
+				return 0
 		return 1
 	
 	def StartCrawling(self,nextUrl=None):
 		if nextUrl==None:
 			nextUrl = self.url
 		html = GetPage(nextUrl)
-		if html !=0 and html !=-1:
-			self.repo.append(nextUrl)
+		
+		if html == -2:
+			html = GetPage(nextUrl)
+		
+		value = [nextUrl,1]
+		if type(html)==type(1):
+			value[1] = html
+			self.repo.append(value)
+		else:
+			self.repo.append(value)
 			self.DoPage(html,nextUrl)
 
 	def Stop(self):
+		#Decides When To Stop
 		if self.count==-1:
 			return 1
 		elif len(self.repo)<=self.count-1:
@@ -79,24 +126,22 @@ class Crawle:
 
 	def DoPage(self,html,currentUrl):
 		if self.Stop()==1:
-			if html != 0:
-				anchorTags = GetAnchorTags(html)
-				hrefs = GetProperLinks(anchorTags,self.url)
-				if self.ext == 1:
-					hrefs = LinkCall(self.url,hrefs)
-				for href in hrefs:
-					if self.CheckList(href)==1:
-						self.StartCrawling(href)
-			elif html == -1:
-				print 'Invalid Content Type'
+			links = GetAllLinks(html,self.url)
+			if self.ext == 1:
+				links = LinkCall(self.url,links)
+			for link in links:
+				if self.CheckList(link)==1:
+					self.StartCrawling(link)
 		else:
 			self.View()
 			sys.exit()
 	
 	def View(self):
+		#Displaying Crawled URL's
 		count = 1
+		print 'SRNO.\tURL\t\t\t\t'
 		for elem in self.repo:
-			print "%d.\t%s"%(count, elem)
+			print "%d.\t%s\t\t\t\t%s"%(count,elem[0],RequestIssue(elem[1]))
 			count += 1
 	
 	def cleanup(*args):
